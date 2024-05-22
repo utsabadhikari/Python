9e40332ddb81663a199b18e32c1f3a68d6921a1e
utsabadhikari <120240456+utsabadhikari@users.noreply.github.com>
2024-05-22 12:44:25 +0545

    Create To define variable.py

---
+++ To define variaable.py
@@ -0,0 +1,4 @@
+# Python can be used to define a variable either an integer or float number.For example in the context of assigning a value to a variable named "price" as 10 we assign as follow:
+price=10
+print(price)
+# The output of the following code will be the assigned value of variable named "price" ,that is 10. 
