946ad2d57fe56b5433cdc65baeee31dbc9a24c59
utsabadhikari <120240456+utsabadhikari@users.noreply.github.com>
2024-05-22 14:19:14 +0545

    Create Type conversion.py

---
+++ Type conversion.py
@@ -0,0 +1,6 @@
+# Python can be used to convert the tye of variables 
+"""
+int() = for conversion of string to a integer
+float() = for conversion of string to flaot 
+bool() =for conversion of string to boolean value
+"""
