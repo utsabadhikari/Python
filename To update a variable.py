fc428c9b54a255961de7ae19f2fe601448c62f2e
utsabadhikari <120240456+utsabadhikari@users.noreply.github.com>
2024-05-22 12:49:48 +0545

    Create To update a variable.py

---
+++ To update a variable.py
@@ -0,0 +1,7 @@
+# Python can also be used to update a varible.As python interpreter executes our code line by line from the top.
+
+#Consider a variable name "selling_price" we assign the valyue to them as follow:
+selling_price=12
+selling_price=20
+print(selling_price)
+#The output of the following code will be the assigned value of variable named "selling_price" ,that is 20 as the variable "selling_price" gets updated in the fifth line as 20 . 
