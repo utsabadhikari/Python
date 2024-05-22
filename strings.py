002c05af04e4a93c9e8804e487c2a9118ad15b76
utsabadhikari <120240456+utsabadhikari@users.noreply.github.com>
2024-05-22 15:10:06 +0545

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,12 @@
+# Strings in Python
+
+# To add apostrophe:
+course ="Python's for Beginners"
+print(course)
+# Output =Python's for Beginners
+
+
+# For double quote:
+course1 ='Python for "Beginners"'
+print(course1)
+# Output= Python for "Beginners"
