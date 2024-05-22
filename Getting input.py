b6d1e64da4ffde502cab528e2392f347cac42378
utsabadhikari <120240456+utsabadhikari@users.noreply.github.com>
2024-05-22 13:23:29 +0545

    Create Getting input.py

---
+++ Getting input.py
@@ -0,0 +1,16 @@
+# Python can be used to recive input from the user
+# Get user's name
+name = input("What is your name?\n")
+print("Hi " + name)
+
+# Get user's favorite color
+color = input("What is your favorite color?\n")
+print(name + " likes " + color)
+
+# Get user's favorite food
+food = input("What is your favorite food?\n")
+print(name + " likes " + food)
+
+# Get user's Date of birth
+birth = int(input("What is your Date of Birth?\n"))
+print(name + " was born in " + str(birth))
